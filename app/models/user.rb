a582d0e03e6a8a1a7f2bccec630f9927210e1cf9
alpizard <kalpizardfwdcostarica.com>
2024-01-05 14:48:42 -0600

    delete model, controller, migrate user

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
- 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
-    has_one :rating, dependent: :destroy
-   
-    has_many :user_notifications
-end
