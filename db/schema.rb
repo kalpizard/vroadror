4f2785ed7bb3d3baa8f56baa8557d64f24f7a5f4
alpizard <kalpizardfwdcostarica.com>
2024-01-09 11:33:09 -0600

    Devise_JWT with SQL SERVER imp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_05_203312) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_09_164726) do
   create_table "advances", force: :cascade do |t|
     t.datetime "death_line"
     t.float "current_progress"
@@ -40,6 +40,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_05_203312) do
     t.string "goal_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -90,11 +96,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_05_203312) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.string "user_email"
-    t.string "user_password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, where: "([email] IS NOT NULL)"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, where: "([reset_password_token] IS NOT NULL)"
   end
 
   add_foreign_key "notifications", "congratulations"
